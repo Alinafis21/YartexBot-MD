9fdc24de1eb56357e53af0ac248d4634fe7fa132
Alinafis21 <dstacee17@freegan.win>
2024-05-27 14:07:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,37 +7,37 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['447700168473', '👑 𝐎𝐰𝐧𝐞𝐫 👑', true], ['5217294888993'], ['5214531287294'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['59162714296'], ['5492964650915'], ['50558124470'], ['5214434703586'], ['18293132203'], ['573003025991']]
+global.owner = [['212618951915', 'ALI NAFIS🍧', true], ['212618951915', '👑 𝐎𝐰𝐧𝐞𝐫 👑', true], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915'], ['212618951915']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '' //Ejemplo: +212618951915
 global.confirmCode = ''
 
-global.suittag = ['5214531287294']
-global.mods = []
-global.prems = []
+global.suittag = ['212618951915']
+global.mods = ['212618951915']
+global.prems = ['212618951915']
 
 //Solo desarrolladores aprobados
-global.isdev = [ /* ['5217294888993'], */ ['5214434703586'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['5492964650915'], ['50558124470']]
+global.isdev = [ /* ['212618951915'], */ ['5214434703586'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['5492964650915'], ['50558124470']]
 
 //Aqui encuentras los nombres del bot 
 global.packname = ''
-global.author = 'YartexBot-MD'
-global.wm = 'Yartex Bot - MD'
-global.wm2 = 'Yartex : Bot'
-global.azami = 'GataNina-Li'
-global.cb = 'YartexBot'
+global.author = 'YORIBOT-MD'
+global.wm = 'YORIBOT - MD'
+global.wm2 = 'YORI : Bot'
+global.azami = 'ALI_NAFIS'
+global.cb = 'ALI_NAFIS'
 
 
 //Las Versiones Y Entre Otros
 global.vs = 'V2'
-global.library = 'Baileys'
-global.baileys = '@whiskeysockets/baileys'
-global.lenguaje = 'Español'
+global.library = 'NAFIS'
+global.baileys = '@Alinafis21'
+global.lenguaje = 'MOROCCO'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = 'GataNina-Li'
-global.devnum = '+593 96 826 3524'
+global.dev = 'ALI NAFIS'
+global.devnum = '+212618951915'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
